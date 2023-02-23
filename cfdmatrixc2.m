cafe5dfa63f980458052b41588e982780fd1c672
Chengyuan Hao <chengyuan.hao@postgrad.manchester.ac.uk>
2023-02-23 15:00:38 +0000

    Delete cfdmatrixc2.m

--- cfdmatrixc2.m
+++
@@ -1,9 +0,0 @@
-function C = cfdmatrixc2(o)
-% matrix C by using one-way nth order discretisation 
-p = o;
-C = zeros(o);
-for m = 0:p
-    for n = 0:p
-        C(m+1,n+1) = ((n)^m)/factorial(m);
-    end
-end
